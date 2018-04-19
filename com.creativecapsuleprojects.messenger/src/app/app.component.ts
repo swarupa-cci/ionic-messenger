b2fc91e4ffae7062d23319a5a48da425bf3ed6cd
Swarupa Vengurlekar <swarupa@creativecapsule.com>
2018-04-19 14:30:15 +0530

    Implemented Splash page

--- com.creativecapsuleprojects.messenger/src/app/app.component.ts
+++ com.creativecapsuleprojects.messenger/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform ,ModalController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import {SplashPage} from '../pages/splash/splash';
 
 @Component({
   templateUrl: 'app.html'
@@ -16,8 +17,16 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public modalCtrl:ModalController) {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      //this.splashScreen.hide();
+      console.log("Platform ready");
+      let splash = modalCtrl.create(SplashPage);
+      splash.present();
+    });
 
     // used for an example of ngFor and navigation
     this.pages = [
